a75afc7e01290dd9f4c217df0a493d50a9ddd065
Mitchell Callaghan <mitchy1898@gmail.com>
2024-11-20 09:06:58 -0500

    Added keyboard functionality

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ hoverButton.addEventListener('mouseover', function() {
 hoverButton.addEventListener('mouseleave', function() {
     hoverButton.textContent = 'This will change';
 });
+
+// Keyboard Event
+const keyboardInput = document.getElementById('keyboardInput');
+const keyPressedMessage = document.getElementById('keyPressedMessage');
+keyboardInput.addEventListener('keydown', function(event) {
+    keyPressedMessage.textContent = `You just pressed the ${event.key} key`;
+});
